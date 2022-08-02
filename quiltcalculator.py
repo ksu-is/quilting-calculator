396382401a9a89a93aac00196edac309237c9adc
Erinfoleyyy <89428343+Erinfoleyyy@users.noreply.github.com>
2022-08-01 22:50:46 -0400

    Update quiltcalculator.py

--- quiltcalculator.py
+++ quiltcalculator.py
@@ -5,27 +5,18 @@ from math import sqrt as sqr
 class calculator(Frame):
 
     def __init__(self, master):
-        """
-        Initializes the frame.
-        :param master: root.Tk()
-        """
+       
         Frame.__init__(self, master)
-        self.entry = Entry(master, width=30, font=("Times New Roman",25))
+        self.entry = Entry(master, width=30, font=("Arial",25))
         self.entry.grid(row=0, column=0, columnspan=1, sticky="w")
         self.entry.focus_set()
-        self.entry.configure(state="disabled", disabledbackground="black", disabledforeground="white")
+        self.entry.configure(state="disabled", disabledbackground="white", disabledforeground="black")
         self.create_widgets()
         self.bind_buttons(master)
         self.grid()
 
     def add_chr(self, char, btn=None):
-        """
-        Concatenates a character passed from a button press (or key type) 
-        to a string.
-        :param char: string to add passed from a button
-        :param btn: button name to use if key is pressed (to flash)
-        :return: None
-        """
+        
         self.entry.configure(state="normal")
         self.flash(btn) # Flash a button correspond to keystroke
         if self.entry.get() == "Invalid Input":
@@ -34,10 +25,7 @@ class calculator(Frame):
         self.entry.configure(state="disabled")
 
     def clear(self):
-        """
-        Allows user to backspace their entry.
-        :return: None
-        """
+        """Button to clear calculator"""
         self.entry.configure(state="normal")
         if self.entry.get() != "Invalid Input":
             # Clears full entry when "Invalid Input"
@@ -49,20 +37,13 @@ class calculator(Frame):
         self.entry.configure(state="disabled")
 
     def clear_all(self):
-        """
-        Allows user to clear the full entry.
-        :return: None
-        """
+        """Button to clear all of calculator"""
         self.entry.configure(state="normal")
         self.entry.delete(0, END)
         self.entry.configure(state="disabled")
 
     def calculate(self):
-        """
-        Changes the operation symbols to their mathematical representation used in 
-        the eval() method.
-        :return: None
-        """
+        """Allows for quilt name to display without visible numbers"""
         self.entry.configure(state="normal")
         e = self.entry.get()
         e = e.replace("Throw","50X60")
@@ -105,37 +86,37 @@ class calculator(Frame):
         master.bind("King", lambda event, char="14", btn=self.king_bttn: self.add_chr(char, btn))
         master.bind("1/4 Seam", lambda event, char=".25", btn=self.quarter_bttn: self.add_chr(char, btn))
         master.bind("1/2 Seam", lambda event, char=".5", btn=self.half_bttn: self.add_chr(char, btn))
-        master.bind("+", lambda event, char="+", btn=self.add_bttn: self.add_chr(char, btn))
+        master.bind("X", lambda event, char="*", btn=self.multiply_bttn: self.add_chr(char, btn))
 
     def create_widgets(self):
-        self.eq_bttn = Button(self, text="=", width=30, height=3, bg="Pink", command=lambda: self.calculate())
+        self.eq_bttn = Button(self, text="Calculate", width=70, height=3, bg="white", fg="black", command=lambda: self.calculate())
         self.eq_bttn.grid(row=4, column=0, columnspan=4)
 
-        self.ac_bttn = Button(self, text='Clear', width=6, height=2, bg='LightBlue', fg='black',command=lambda: self.clear_all())
+        self.ac_bttn = Button(self, text='Clear', width=22, height=3, bg='Lightgreen', fg='black',command=lambda: self.clear_all())
         self.ac_bttn.grid(row=1, column=2)
 
-        self.c_bttn = Button(self, text='←', width=6, height=2, bg='LightBlue', fg='black',command=lambda: self.clear())
+        self.c_bttn = Button(self, text='←', width=22, height=3, bg='Lightgreen', fg='black',command=lambda: self.clear())
         self.c_bttn.grid(row=2, column=2 )
 
-        self.add_bttn = Button(self, text="x", width=6, height=2,bg='LightBlue', fg='black', command=lambda: self.add_chr('*'))
-        self.add_bttn.grid(row=3, column=2)
+        self.multiply_bttn = Button(self, text="X", width=22, height=3,bg='Lightgreen', fg='black', command=lambda: self.add_chr('*'))
+        self.multiply_bttn.grid(row=3, column=2)
 
-        self.throw_bttn = Button(self, text="Throw", width=6, height=2,bg='LightBlue', fg='black', command=lambda: self.add_chr('5'))
+        self.throw_bttn = Button(self, text="Throw", width=22, height=3,bg='Lightgreen', fg='black', command=lambda: self.add_chr('5'))
         self.throw_bttn.grid(row=2, column=0)
 
-        self.full_bttn = Button(self, text="Full", width=6, height=2, bg='LightBlue', fg='black',command=lambda: self.add_chr('8'))
+        self.full_bttn = Button(self, text="Full", width=22, height=3, bg='Lightgreen', fg='black',command=lambda: self.add_chr('8'))
         self.full_bttn.grid(row=2, column=1)
 
-        self.queen_bttn = Button(self, text="Queen", width=6, height=2,bg='LightBlue', fg='black', command=lambda: self.add_chr('12'))
+        self.queen_bttn = Button(self, text="Queen", width=22, height=3,bg='Lightgreen', fg='black', command=lambda: self.add_chr('12'))
         self.queen_bttn.grid(row=3, column=0)
 
-        self.king_bttn = Button(self, text="King", width=6, height=2,bg='LightBlue', fg='black', command=lambda: self.add_chr('14'))
+        self.king_bttn = Button(self, text="King", width=22, height=3,bg='Lightgreen', fg='black', command=lambda: self.add_chr('14'))
         self.king_bttn.grid(row=3, column=1)
 
-        self.quarter_bttn = Button(self, text="1/4 Seam", width=6, height=2, bg='LightBlue', fg='black',command=lambda: self.add_chr(".25"))
+        self.quarter_bttn = Button(self, text="1/4 Seam", width=22, height=3, bg='Lightgreen', fg='black',command=lambda: self.add_chr(".25"))
         self.quarter_bttn.grid(row=1, column=0)
 
-        self.half_bttn = Button(self, text="1/2 Seam", width=6, height=2, bg='LightBlue', fg='black',command=lambda: self.add_chr(".5"))
+        self.half_bttn = Button(self, text="1/2 Seam", width=22, height=3, bg='Lightgreen', fg='black',command=lambda: self.add_chr(".5"))
         self.half_bttn.grid(row=1, column=1)
 
 root = Tk()
@@ -143,4 +124,6 @@ root.geometry()
 root.title("Calculator")
 app = calculator(root)
 root.mainloop()
+
+
 
